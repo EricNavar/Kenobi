de02b68d3982b3f7a25e3c3baeca808354430ad2
Eric N <32403644+Tommorow@users.noreply.github.com>
2018-02-06 19:45:04 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+var Discordie = require("discordie");
+var Events = Discordie.Events;
+
+var client = new Discordie();
+
+//client.connect({ token: "NDA5Nzk3NTA1ODI0NDU2NzA2.DVo2cg.Vwd2JZ_7pG6MUs0_RXZ31aVMeWA" });
+
+
+client.Dispatcher.on(Events.GATEWAY_READY, e => {
+console.log("Connected as: " + client.User.username);
+});
+
+client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
+if (e.message.content.toLowerCase() == "hello there")
+e.message.channel.sendMessage("General Kenobi");
+});
+
+client.login(process.env.BOT_TOKEN);
