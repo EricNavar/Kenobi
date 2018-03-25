7f8921b4c123ad0867e2936558b6909bb55015a6
Eric N <32403644+Tommorow@users.noreply.github.com>
2018-03-25 16:25:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,20 @@ client.on('message', message => {
     if (!message.author.bot){
         content = message.content.toLowerCase()
         
-        if (message.content.toLowerCase() === 'hello there') {
+        if (content === 'hello there') {
     	message.reply('General Kenobi!');
         }   else if (content.includes('anime')) {
-            message.reply('Anime is trash. Get help.');
-            message.delete();
-        }   
-        
+            if (!content.equals("anime") && !content.equals("Anime is trash")){
+                message.reply('Anime is trash. Get help.');
+                message.delete();
+            } else if (message.author.username.equals("a")) {
+                message.reply('Anime is trash. Get help.');
+            }
+                
+            
+        }   else if (content.equals("username"))
+            message.reply(message.author.username);
+        }
         
     }
 
